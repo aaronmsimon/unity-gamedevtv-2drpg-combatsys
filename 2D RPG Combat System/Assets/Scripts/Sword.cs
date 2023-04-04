4f3a7d5fe919e38d108632d7a029a1fcd5066c50
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2023-04-03 21:16:58 -0700

    Swing Both Directions (4_CB_UPC)

--- 2D RPG Combat System/Assets/Scripts/Sword.cs
+++ 2D RPG Combat System/Assets/Scripts/Sword.cs
@@ -6,10 +6,14 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator swordAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake() {
         playerControls = new PlayerControls();
         swordAnimator = GetComponent<Animator>();
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
     }
 
     private void OnEnable() {
@@ -20,7 +24,20 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update() {
+        MouseFollowWithOffset();
+    }
+
     private void Attack() {
         swordAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        activeWeapon.transform.rotation = Quaternion.Euler(0, mousePos.x < playerScreenPoint.x ? 180f : 0f, angle);
     }
 }
