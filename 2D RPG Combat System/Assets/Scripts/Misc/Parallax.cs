f23a06fdf8d5bff1e198c771d072440ce1e956d6
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2023-04-09 15:26:10 -0700

    Parallax (7_EN_UPC)
    Creating parallax effect.
    This is done by offsetting the camera by calculating the difference in starting position and the current position of the camera (which is following the player) and multiplying it by an offset factor.
    Note that this example also demonstrates casting data types.

---
+++ 2D RPG Combat System/Assets/Scripts/Misc/Parallax.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float parallaxOffset = -.15f;
+
+    private Camera cam;
+    private Vector2 startPos;
+    private Vector2 travelDist => (Vector2)cam.transform.position - startPos;
+
+    private void Awake() {
+        cam = Camera.main;
+    }
+
+    private void Start() {
+        startPos = transform.position;
+    }
+
+    private void FixedUpdate() {
+        cam.transform.position = startPos + travelDist * parallaxOffset;
+    }
+}
