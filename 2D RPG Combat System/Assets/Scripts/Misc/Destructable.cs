e48c021918adb468b9dc8890b5adf62abd49991c
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2023-04-09 23:41:07 -0700

    Destructible Objects (8_EN_UPC)
    Create Destructable script to be able to destroy any item with a custom VFX
    Used a new leaf sprite to create this effect using a variant form the other Particle System made earlier.  Tweaks included rotation in the Z-axis, making the size 100%, and disabling shrinking the size over time

---
+++ 2D RPG Combat System/Assets/Scripts/Misc/Destructable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    [SerializeField] private GameObject destroyVFX;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.GetComponent<DamageSource>()) {
+            Instantiate(destroyVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
