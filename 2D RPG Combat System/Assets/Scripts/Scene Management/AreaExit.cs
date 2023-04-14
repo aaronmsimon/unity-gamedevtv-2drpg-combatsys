c6448aaf06b82ccf82575d527b9f2dd9e0c0798a
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2023-04-13 19:45:27 -0700

    Scene Entry (4_PS_UPC)
    Set up SceneManagement as a singleton to handle transitions

--- 2D RPG Combat System/Assets/Scripts/Scene Management/AreaExit.cs
+++ 2D RPG Combat System/Assets/Scripts/Scene Management/AreaExit.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 public class AreaExit : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
+    [SerializeField] private string sceneTransitionName;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.GetComponent<PlayerController>()) {
+            SceneManagement.Instance.SetTransitionName(sceneTransitionName);
             SceneManager.LoadScene(sceneToLoad);
         }
     }
